f7ff8d1248c22fea5ad921d110563012122f307e
ruizhiwang11 <43807154+ruizhiwang11@users.noreply.github.com>
2021-11-18 20:28:36 +0800

    add cards

---
+++ cards/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades" // dynamic data binding, same as python
+	//card = "Five of Diamonds" // change a variable
+	//fmt.Println(card)
+	// cards := deck{"Ace of Diamonds", newCard()}
+	// cards = append(cards, "Six of Spades")
+
+	cards := newDeck()
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+	hand, remaining_deck := deal(cards, 5)
+	hand.print()
+	remaining_deck.print()
+}
+
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
